c6c797fd40587e578ea84fed450dacd8fee44027
grabeqx <grabowski.kontakt@gmail.com>
2018-01-27 16:24:32 +0100

    Add layout to home page

--- dist/index.bundle.js
+++ dist/index.bundle.js
@@ -65,9 +65,15 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(1);
+
+/***/ }),
+/* 1 */
 /***/ (function(module, exports) {
 
-
+// removed by extract-text-webpack-plugin
 
 /***/ })
 /******/ ]);
